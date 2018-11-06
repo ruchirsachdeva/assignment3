ad1adbe54c56b4576e29284bfdf11b5fef3528ce
Ruchir <Ruchir.Sachdeva@kindredgroup.com>
2018-11-06 17:39:41 +0100

    Added a scheduled task to ping Heroku apps every 20 minutes to stop them from sleeping after 30 minutes of inactivity.

--- src/main/java/com/lnu/foundation/Assignment2Application.java
+++ src/main/java/com/lnu/foundation/Assignment2Application.java
@@ -4,8 +4,10 @@ import com.lnu.foundation.config.SecurityConfig;
 import com.lnu.foundation.config.WebConfig;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Assignment2Application {
 
 	public static void main(String[] args) {
