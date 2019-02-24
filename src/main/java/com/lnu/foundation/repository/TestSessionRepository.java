57ae45e17dd9b9cf9ffd3808f0655ab9aa56eca6
Ruchir Sachdeva <ruchirsachdeva@yahoo.com>
2019-02-25 00:58:47 +0100

    rest repository params set

--- src/main/java/com/lnu/foundation/repository/TestSessionRepository.java
+++ src/main/java/com/lnu/foundation/repository/TestSessionRepository.java
@@ -2,7 +2,9 @@ package com.lnu.foundation.repository;
 
 import com.lnu.foundation.model.TestSession;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Collection;
 
@@ -15,4 +17,11 @@ public interface TestSessionRepository extends JpaRepository<TestSession, Long> 
     Collection<TestSession> findByTest_Therapy_Med_Username(String username);
 
     Collection<TestSession> findByTest_Therapy_Patient_Username(String username);
+
+    @RestResource(path = "byMedId", rel = "byMedId")
+    Collection<TestSession> findByTest_Therapy_Med_UserId(@Param("id") Long id);
+
+
+    @RestResource(path = "byPatientId", rel = "byPatientId")
+    Collection<TestSession> findByTest_Therapy_Patient_UserId(@Param("id") Long id);
 }
--- src/main/java/com/lnu/foundation/repository/UserRepository.java
+++ src/main/java/com/lnu/foundation/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.lnu.foundation.repository;
 
 import com.lnu.foundation.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +15,7 @@ import java.util.Optional;
 @RepositoryRestResource
 //@CrossOrigin(origins = {"http://localhost:4200", "https://lit-beach-29911.herokuapp.com"})
 public interface UserRepository extends JpaRepository<User, Long> {
-    List<User> findByRole_Name(String role);
-    Optional<User> findByUsername(String email);
+    List<User> findByRole_Name(@Param("role")String role);
+    @RestResource
+    Optional<User> findByUsername(@Param("username")String email);
 }
